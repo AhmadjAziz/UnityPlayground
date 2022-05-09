ab52d78397c78c2e56f93d3bc92b6c030a8bc295
AhmadAziz0090 <72610079+AhmadAziz0090@users.noreply.github.com>
2022-05-09 23:32:48 +0100

    Created blend tree and worked on animation script

--- Assets/Scripts/AnimationStateController.cs
+++ Assets/Scripts/AnimationStateController.cs
@@ -5,14 +5,37 @@ using UnityEngine;
 public class AnimationStateController : MonoBehaviour
 {
     private Animator animator;
-
+     int velocityHash;
+    [SerializeField]
+    private float velocity;
+    [SerializeField]
+    private float acceleration;
+    [SerializeField]
+    private float deceleration;
     private void Awake()
     {
         animator = GetComponent<Animator>();
+        velocityHash = Animator.StringToHash("Velocity");
     }
 
     private void Update()
     {
-        
+        bool forwardPressed =Input.GetKey(KeyCode.W);
+        bool runPressed = Input.GetKey(KeyCode.LeftShift);
+        if (forwardPressed && velocity < 5.0f)
+        {
+            velocity += Time.deltaTime * acceleration;
+        }
+
+        if (!forwardPressed && velocity > 0.0f)
+        {
+            velocity -= Time.deltaTime * deceleration;
+        }
+
+        if (!forwardPressed && velocity < 0.0f)
+        {
+            velocity = 0.0f;
+        }
+            animator.SetFloat(velocityHash, velocity);
     }
 }
